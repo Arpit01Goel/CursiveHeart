e8876d2bf23690919d994641d03700482714e532
Arpit <Arpit01Goel@gmail.com>
2025-06-11 19:45:40 +0530

    add redirecitng file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Welcome from "./Components/Welcome.jsx";
 
 function App() {
   // console.log("hello");
-  console.log("API_BASE_URL:", import.meta.env.VITE_API_BASE_URL); // Log the API base URL for debugging
+  // console.log("API_BASE_URL:", import.meta.env.VITE_API_BASE_URL); // Log the API base URL for debugging
   return (
     <Router>
       <Routes>
