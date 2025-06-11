33bcc10a394398bef71c355daba7184aad8df884
Arpit <Arpit01Goel@gmail.com>
2025-06-11 20:21:26 +0530

    updated welcome page

--- frontend/src/Components/Welcome.jsx
+++ frontend/src/Components/Welcome.jsx
@@ -1,9 +1,18 @@
-function Welcome()  {
+import { useNavigate } from 'react-router-dom';
+
+function Welcome() {
+    const navigate = useNavigate();
+
+    const handleRedirect = () => {
+        navigate('/Auth');
+    };
+
     return (
         <>
             Welcome to my application!
+            <button onClick={handleRedirect}>Go to Auth Page</button>
         </>
-    )
+    );
 }
 
 export default Welcome;
